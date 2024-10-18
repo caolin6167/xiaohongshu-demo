977cac8c0925092b9d55aaa5aa623e4103dd931d
cao.changhu <1070469321@qq.com>
2024-10-18 11:21:57 +0800

    1

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"wechatgame",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/yxt.fire",orientation:"",server:"",jsList:[]};
